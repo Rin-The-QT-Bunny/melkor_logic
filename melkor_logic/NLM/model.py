e7405e876ef4a529b5f9ae2ef3261d29e80b21c8
Melkor <rintfd@163.com>
2022-09-09 08:10:24 +0800

    update

---
+++ melkor_logic/NLM/model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+
+class PermuteLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+class ExpandLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+class NeuroLogicMachine(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self,x):return x
--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from melkor_logic.TOQNet import *
 
 T=25;d0=8;d1=10;d2=12;N = 4;b=3
-config1 = (
+spatial_config = (
     (8,10,12),
     (10,12,14),
     (6,8,4),
     )
 
-config2 = (
+temporal_config = (
     [24,28],
     [28,30],
     [30,32]
@@ -17,7 +17,7 @@ P = torch.randn([b,T,d0])
 Q = torch.randn([b,T,N,d1])
 R = torch.randn([b,T,N,N,d2])
 
-net = TOQNet(config1,config2)
+net = TOQNet(spatial_config,temporal_config)
 
 outputs = net(P,Q,R)
 print(outputs.shape)
